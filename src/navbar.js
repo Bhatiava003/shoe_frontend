eae23589a49c81472560d341d559dbf2300e4eea
unknown <bhatia2003varun@gmail.com>
2023-09-18 14:55:38 +0530

    updated

--- src/navbar.js
+++ src/navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
         </div>
 
         {/* Navigation List */}
-        <ul className="hidden md:flex space-x-4 ml-2">
+        <ul className="hidden md:flex space-x-4 ml-0.5">
           <li>
            <a href="#" className="text-white hover:text-gray-200">Home</a>
           </li>
