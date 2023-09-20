7da1890a23afa72f37d98b834fcef831f2f4c8f4
unknown <bhatia2003varun@gmail.com>
2023-09-20 20:32:59 +0530

    updtaed

--- src/Home.js
+++ src/Home.js
@@ -44,10 +44,10 @@ function Home() {
       </span>
       </div>
 
-      <div className="bg-cover bg-no-repeat bg-center h-screen mt-4 mb-10" style={backgroundImageStyle3}>
+      <div className="bg-cover bg-no-repeat bg-center h-screen mt-4 mb-6" style={backgroundImageStyle3}>
 
       </div>
-      <div className="text-black text-10xl dark:text pt-56 font black"> We Have Awsome Variety In Reebok Shoes </div>
+      <div className="text-black text-7xl dark:text pt-56 font black"> We Have Awsome Variety In Reebok Shoes </div>
 
       
 
