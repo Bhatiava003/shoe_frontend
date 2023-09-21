d67487a5da9fafc5444ae1ed851b42cdb7f051b8
unknown <bhatia2003varun@gmail.com>
2023-09-21 11:12:54 +0530

    updtaed

--- src/footer.js
+++ src/footer.js
@@ -4,6 +4,7 @@ function Footer() {
     return (
       <footer className="bg-gray-800 text-white p-4">
         <div className="container mx-auto text-center">
+            Follow us on:
         </div>
       </footer>
     );
