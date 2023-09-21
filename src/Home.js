4ee345cb3039a7ed5d4cb544e4e9cd2f5fb38a62
unknown <bhatia2003varun@gmail.com>
2023-09-21 20:51:42 +0530

    updated

--- src/Home.js
+++ src/Home.js
@@ -67,7 +67,7 @@ function Home() {
       <div className="bg-cover bg-no-repeat bg-center h-screen w-3/6 mt-4 ml-auto  " style={backgroundImageStyle3}>
            
       </div>
-      <span className='text-black text-5xl dark:text-white pt-56 font-black '>
+      <span className=' mr-auto w-3/6 text-black text-5xl dark:text-white pt-56 font-black '>
         AIR Jordans 1
       </span>
        <div className="mt-56">
