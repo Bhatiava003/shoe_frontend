5cd88d2c2eaaac3a195c45288f30e45726c33bea
unknown <bhatia2003varun@gmail.com>
2023-09-21 19:53:46 +0530

    updtaed

--- src/Home.js
+++ src/Home.js
@@ -55,7 +55,7 @@ function Home() {
        </div>
       </div>
 
-      <div className="bg-neutral-400 bg-no-repeat bg-center h-screen w-3/6 mt-4 ml-auto bg-neutral-400" style={backgroundImageStyle3}>
+      <div className="bg-amber-300 bg-no-repeat bg-center h-screen w-3/6 mt-4 ml-auto bg-neutral-400" style={backgroundImageStyle3}>
            
       </div>
 
