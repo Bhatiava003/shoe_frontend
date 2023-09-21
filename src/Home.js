f97faa4084a74dd4d626d735c3e5651ec6160bf7
unknown <bhatia2003varun@gmail.com>
2023-09-21 12:32:56 +0530

    updated

--- src/Home.js
+++ src/Home.js
@@ -43,7 +43,7 @@ function Home() {
           Nike Air Force 1 x Carhartt WIP
       </span>
 
-       <div className="mt-96 pr-52">
+       <div className="mt-64 pr-2.5">
         <button className='bg-black hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-md text-4xl'>Shop</button>
        </div>
       </div>
