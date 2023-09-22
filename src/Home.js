8672d82ddba5c7e8ada06ae515141299b11d2556
unknown <bhatia2003varun@gmail.com>
2023-09-22 12:38:38 +0530

    updated

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ function Home() {
     backgroundImage: 'url("https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2012&q=80")',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'left',
+    backgroundPosition: 'center',
     
   };
 
@@ -24,17 +24,11 @@ function Home() {
     backgroundImage: 'url("https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/2b894584-ce57-467a-a3d7-e3ed5264d87e/air-jordan-1-mid-se-shoes-Q2sGS7.png")',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'right',
+    backgroundPosition: 'center',
   };
 
+  
   const backgroundImageStyle4 = {
-    backgroundImage: 'url("https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/99af6dea-fbad-43d0-983b-ac3197a4ea70/air-jordan-1-mid-shoes-86f1ZW.png")',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'left',
-  };
-
-  const backgroundImageStyle5 = {
     backgroundImage: 'url("https://cdn.sanity.io/images/c1chvb1i/production/53fa76a8d1edbc69650a164e69d265265ce450d8-1100x735.jpg/reebok-classic-leather-2022-1.jpg")',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
@@ -74,20 +68,15 @@ function Home() {
       <div className="bg-cover bg-no-repeat bg-center h-screen w-1/2 mt-4 ml-auto " style={backgroundImageStyle3}>
            
       </div>
-      <span className=''>
-      </span>
+      <span className='w-1/2 text-5xl text-black text-left'> Air Jordan 1 </span>
        <div className="">
         <button className=''>  </button>
        </div>
 
       </div>
 
-     <div className='flex flex-row'>
-     <div className="bg-cover bg-no-repeat bg-center h-screen w-1/2 mt-4 " style={backgroundImageStyle4}>
-           
-     </div>
-     </div>
-      <div className="bg-cover bg-no-repeat bg-center h-screen mt-4 mb-10" style={backgroundImageStyle5}>
+     
+      <div className="bg-cover bg-no-repeat bg-center h-screen mt-4 mb-10" style={backgroundImageStyle4}>
 
       </div>
       <div className="text-black text-bold text-7xl dark:text ml-20 mb-10 font black"> We Have Awesome Variety In Reebok Shoes </div>
